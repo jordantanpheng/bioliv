b3f314a1acd6fbea1724baab73c07bf3dedce0ff
jordantanpheng <jordan.tanp@gmail.com>
2021-04-19 16:37:00 +0200

    new files

---
+++ jstore-angular/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  API: 'https://api-ecommerce.espero-soft.com/api/',
+  //API: 'http://localhost/ecommerce/backend/api/',
+  API_KEY: 'API_KEY=adsffsdfds6b-6727-46f4-8bee-2c6ce6293e41',
+  prefUrlImage: 'https://api-ecommerce.espero-soft.com/images/products/',
+  //prefUrlImage: 'http://localhost/ecommerce/backend/images/products/',
+  ID_CLIENT_PAYPAL: 'AaMJbn8uaYJ1PuV6_53UnrZ52nChVn_1A1fcAqEX57LL1w0Z-cdo4VUqbQwwU-WEzzMkoTerVHHS0II4',
+  CURRENCY: 'EUR'
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
